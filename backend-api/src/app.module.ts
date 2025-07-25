d699f897fe44838acce56aac377deaff56ed78b4
Bùi Thanh Bình <ThanhBinhBui0511@gmail.com>
2025-07-25 14:52:40 +0700

    .

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -13,8 +13,8 @@ import { ImageSliderModule } from './image-slider/image-slider.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Vina'),
-    // MongooseModule.forRoot('mongodb://10.170.215.79:80/Vina'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/Vina'),
+    MongooseModule.forRoot('mongodb://10.170.215.79:80/Vina'),
     // MongooseModule.forRoot('mongodb+srv://leducminhquan293:leducminhquan293@vinahealthcare.d29qums.mongodb.net/'),
     // MongooseModule.forRoot('mongodb+srv://buithanhbinh0511:AShNvqlMPhhy3sxr@cluster0.nwywlfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
     UsersModule,
